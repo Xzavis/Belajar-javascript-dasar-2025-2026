48e02c235ed933060b78b5119e3a667cd1f3f054
Xzavis <xzaabdulmalikibrahim354@gmail.com>
2025-12-07 19:07:28 +0700

    belajar escape sequense

---
+++ tgl 7,12,2025/escape sequense/script.js
@@ -0,0 +1,6 @@
+ document.writeln("<textarea cols='50' rows='10'>");
+        document.writeln("Xza \nAbdul \nMalik");
+        document.writeln("C:\\home\\joeyx");
+        document.writeln(" \"ini adalah contoh tanda kutip ganda\" ");
+        document.writeln("\'ini adalah contoh tanda kutip single\'");
+        document.writeln("</textarea>");
