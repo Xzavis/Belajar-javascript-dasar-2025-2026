1bbeed7de0f53c5d01dbff8f6f5a050c847f89f2
Xzavis <xzaabdulmalikibrahim354@gmail.com>
2025-12-07 19:35:53 +0700

    variable var, let, const

---
+++ tgl 7,12,2025/variable/script.js
@@ -0,0 +1,24 @@
+// variable fullame
+const fullname = "Xza Abdul Malik Ibrahim";
+const namaDepan = "Xza";
+const namaTengah = "Abdul";
+const namaAkhir = "Malik";
+
+const nama = "Nama: "
+document.writeln(nama + namaDepan + ' ' + namaTengah + ' ' + namaAkhir);
+
+document.writeln("<br>");
+document.writeln("<br>");
+
+// let namaLengkap = "Xza Abdul Malik Ibrahim";
+// const dataKTP = document.writeln(nama + namaLengkap);
+
+// let namaLengkap = "awdawdaw";  --> akan menghasilakan error 
+
+var namaLengkap = "Xza Abdul Malik Ibrahim";
+document.writeln("Sebelum | " + nama + namaLengkap);
+
+document.writeln("<br>");
+
+var namaLengkap = "Vladikov ";
+document.writeln("Sesudah | " + nama + namaLengkap);
